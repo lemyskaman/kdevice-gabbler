8fd4f065fc3565571790923d0450a5259483c3b2
lemys <lemyskaman@gmail.com>
2022-09-07 07:42:13 -0400

    network interfaces list done

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # kdevice-gabbler
+
+A small http service that expose a device hardware info in json
+
+
+### Just:
+
+clone and run:
+
+    npm start
+
+
+or if you are on linux run as root: 
+
+
+    ./install_device-gabbler
+
+the above script will create a copy on your opt directory and them creates a systemd unit service to run the kdevice-gabbler script as daemon  
---
+++ kdevice-gabbler.js
@@ -0,0 +1,45 @@
+'use strict';
+const { networkInterfaces, hostname } = require('os');
+
+
+var os = require("os");
+const nets = networkInterfaces();
+
+
+
+
+const express = require('express');
+const { json } = require('express/lib/response');
+const app = express()
+const port = 7777
+
+
+
+app.get('/', (req, res) => {
+    const interfaces = Object.create(null); // Or just '{}', an empty object
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+            // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+            const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+            if (net.family === familyV4Value && !net.internal) {
+                if (!interfaces[name]) {
+                    interfaces[name] = {};
+                }
+
+
+                interfaces[name][net.mac] = net.address
+
+            }
+        }
+    }
+
+    res.json({
+        "hostname": hostname(),
+        "netinterfaces": interfaces
+    })
+})
+
+app.listen(port, () => {
+    console.log(`kdevice-gabbler  is listening at port: ${port}`)
+})
